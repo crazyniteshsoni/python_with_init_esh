f8cac0b061e551f358bb008faef7c05a5246a86b
init_esh <nitesh.15jics734@jietjodhpur.ac.in>
2019-07-29 14:57:59 +0530

    Update List.py

--- Basic/List.py
+++ Basic/List.py
@@ -32,7 +32,64 @@ def main():
 
 		#output => 1 2 5 6 7 8
 
-	
+	#nested list, we can use list with in list
 
+	list1=[[1,2,3],[4,5],[6,7]]
+
+	print(list1[0][1])		#list1[0][1] => [1,2,3] => 2
+	print(list1[1][1])		#list1[1][1] => [4,5] => 5
+
+	############     list buildin functions    #################
+	#add data at end of the list => append
+	tmplist=list()			#make empty list
+	tmplist.append(1)		#add 1 in tmplist
+	tmplist.append('Hello')	#add 'Hello' in tmplist
+	tmplist.append([2,3,4])	#add [2,3,4] in tmplist
+	print(tmplist)			#output => [1,'Hello',[2,3,4]]
+
+
+	#add data at specific position =>insert
+	tmplist.insert(2,'World')		#insert 'World' at position 2
+	tmplist.insert(-1,'Ronak')		#insert 'Ronak' at second last position
+
+	#add another list at end => extend
+	tmplist.extend(names)			#extend tmplist and add names at end
+
+	#find index of element in list
+	tmplist.index("World")			#return the index position of given element
+
+	#delete last element and retrun the deleted element => pop
+	tmplist.pop()					#delete the last element
+
+	#delete element from list ( 1st instance only)
+	tmplist.remove("Hello")
+
+	#delete element from list with given index position
+	tmplist.pop(2)					#delete 2nd element from list
+
+	#sort the list
+	tmplist.sort()
+
+	#reverse the list
+	tmplist.reverse()
+
+	#count the occerence of element in list
+	tmplist.count("Ronak")
+
+	#copy a list without reference
+	#with pointer
+	a=[1,2,3]
+	b=a 		#b is just point to a if we add element in a it also reflects b
+	c=a.copy() 	#c is not pointer to a, changes in a doesn't reflect c
+
+
+
+
+
+	#remove all elements => clear
+	tmplist.clear()
+
+
+	print(tmplist)			#output => [1,'Test']
 if __name__ == '__main__':
 	main()
